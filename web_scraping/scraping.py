2ddf42a1a16981aebd4fc1ad5bbcded6d586c605
Andrew Sturges <arsturges@lbl.gov>
2012-09-12 15:20:03 -0700

    removed web scraping

--- web_scraping/scraping.py
+++
@@ -1,8 +0,0 @@
-import urllib2
-# $ sudo apt-get install libxml libxml-dev libxslt-dev
-# $ sudo easy_install lxml
-from lxml import etree
-doe_database_url = "http://www.regulations.doe.gov/certification-data/Category.html"
-doe_database_text = urllib2.urlopen(doe_database_url)
-source = doe_database_text.read()
-doe_database_text.close()
