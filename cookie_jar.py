c5217324940cb262fcd5dc495b366263eadb8000
Andrew Sturges <arsturges@lbl.gov>
2012-09-14 12:56:43 -0700

    cookie jar

--- cookie_jar.py
+++ cookie_jar.py
@@ -1,3 +1,4 @@
+import numpy as np
 class CookieJar:
 	num_pennies = 0
 	num_nickels = 0
@@ -5,5 +6,19 @@ class CookieJar:
 	num_quarters = 0
 	num_transactions_completed = 0
 
-	#calc_change(3.45) → {"quarter": 2, "dime": 0, "nickel": 1, "penny": 0}
-	def calc_change(amt):
+
+def calc_change(amt):
+	change = np.ceil(amt) - amt
+	print(change)
+	num_quarters = int(np.floor(change/0.25))
+	change = change - num_quarters * 0.25
+	print(change)
+	num_dimes = int(np.floor(change/0.10))
+	change = change - num_dimes * 0.10
+	print(change)
+	num_nickels = int(np.floor(change/0.05))
+	change = change - num_nickels * 0.05
+	print(change)
+	num_pennies = int(change * 100)
+	coins = {"quarter": num_quarters, "dime": num_dimes, "nickel": num_nickels, "penny": num_pennies}
+	return coins
